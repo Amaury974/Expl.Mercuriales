76541dcb748445b4176ac61059fff394cb19d452
Amaury Jorant <amaury.jorant@reunion.chambagri.fr>
2026-01-26 13:43:25 +0400

    maj install

--- installation packages.R
+++ installation packages.R
@@ -2,8 +2,8 @@
 
 
 
-install.packages(c('shiny',
-                   'ggplot2',
-                   'dplyr',
-                   'shinyWidgets',
-                   'RColorBrewer'))
+packages <- c('shiny','ggplot2','dplyr','shinyWidgets','RColorBrewer')
+packages_manquants <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(packages_manquants) > 0) {
+  install.packages(packages_manquants)
+}
